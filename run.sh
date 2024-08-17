878daa6b28f4bf7c1151f6f61d3e46c280fa009b
enkoro <me@nekoro.space>
2024-08-18 01:33:29 +0300

    feat: nginx daemon off

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@
 export PH=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13)
 export PD=$(openssl rand -base64 32)
 sh /app/cfg.json.template >  /app/cfg.json
-/docker-entrypoint.sh nginx
+/docker-entrypoint.sh nginx -g 'daemon off;'
