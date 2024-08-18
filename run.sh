bc7a643b8cb5e65dc4e52ec97c517d50d0e2bb5e
enkoro <me@nekoro.space>
2024-08-18 11:26:01 +0300

    feat: logging

--- run.sh
+++ run.sh
@@ -3,5 +3,5 @@
 export PH=$(tr -dc A-Za-z0-9 </dev/urandom | head -c 13)
 export PD=$(openssl rand -base64 32)
 sh /app/cfg.json.template >  /app/cfg.json
-/app/app -c /app/cfg.json && \
+nohup /app/app -c /app/cfg.json 2>&1 > /app/app.log &
 /docker-entrypoint.sh nginx -g 'daemon off;'
